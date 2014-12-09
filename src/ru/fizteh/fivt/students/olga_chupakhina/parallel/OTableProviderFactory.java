8db3c0dfcda3499269da7a6e06fbeaf87bfe7e7a
ol5478 <ol5478@yandex.ru>
2014-12-09 15:20:38 +0300

    Parallel

---
+++ src/ru/fizteh/fivt/students/olga_chupakhina/parallel/OTableProviderFactory.java
@@ -0,0 +1,17 @@
+package ru.fizteh.fivt.students.olga_chupakhina.parallel;
+
+import ru.fizteh.fivt.storage.structured.TableProvider;
+import ru.fizteh.fivt.storage.structured.TableProviderFactory;
+
+import java.io.IOException;
+
+public class OTableProviderFactory implements TableProviderFactory {
+
+    @Override
+    public TableProvider create(String path) throws IOException {
+        if (path == null) {
+            throw new IllegalArgumentException("Directory name is null");
+        }
+        return new OTableProvider(path);
+    }
+}
